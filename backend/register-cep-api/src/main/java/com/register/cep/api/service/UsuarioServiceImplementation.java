84d1990215aa6292b1be8c73d3555836e9685982
andreluislima <andrelimag89@gmail.com>
2026-03-07 16:00:31 -0300

    feat(service): adiciona a persistencia dos dados da api via-cep

--- backend/register-cep-api/src/main/java/com/register/cep/api/service/UsuarioServiceImplementation.java
+++ backend/register-cep-api/src/main/java/com/register/cep/api/service/UsuarioServiceImplementation.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.register.cep.api.domain.Usuario;
+import com.register.cep.api.dto.ApiCepResponseDTO;
 import com.register.cep.api.dto.UsuarioRequestDTO;
 import com.register.cep.api.repository.UsuarioRepository;
 
@@ -15,17 +16,30 @@ public class UsuarioServiceImplementation implements UsuarioServiceInterface{
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private ApiCepServiceInterface apiCepServiceInterface;
+	
+	
 
 	@Override
 	public Usuario criarUsuario(UsuarioRequestDTO dto) {
+		
 		if(usuarioRepository.findByCpf(dto.cpf()).isPresent()) {
 			throw new RuntimeException(dto.cpf() + "CPF já cadastrado.");
 		}
+		
+		ApiCepResponseDTO endereco = apiCepServiceInterface.buscarEnderecoPorCep(dto.cep());
 		
 		Usuario user  = new Usuario();
 		user.setCep(dto.cep());
 		user.setCpf(dto.cpf());
 		user.setNome(dto.nome());
+		
+		user.setLogradouro(endereco.logradouro());
+		user.setBairro(endereco.bairro());
+		user.setCidade(endereco.cidade());
+		user.setEstado(endereco.estado());
+		
 		user.setDataCriacao(user.getDataCriacao().now());
 		user.setDataAtualizacao(user.getDataAtualizacao().now());
 		
