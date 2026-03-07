241571a120172dff6c45757d687e69ff4148267c
andreluislima <andrelimag89@gmail.com>
2026-03-07 12:24:41 -0300

    feat(domain): cria entidade Usuario

---
+++ backend/register-cep-api/src/main/java/com/register/cep/api/domain/Usuario.java
@@ -0,0 +1,137 @@
+package com.register.cep.api.domain;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+@SequenceGenerator(name="user_id_seq", sequenceName = "user_id_seq", initialValue = 1, allocationSize = 1)
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="cpf", unique = true )
+	private String cpf;
+	
+	@Column(name="cep")
+	private String cep;
+	
+	@Column(name="logradouro")
+	private String logradouro;
+	
+	@Column(name="bairro")
+	private String bairro;
+	
+	@Column(name="cidade")
+	private String cidade;
+	
+	@Column(name="estado")
+	private String estado;
+	
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	@Column(name="data_criacao")
+	private LocalDateTime dataCriacao;
+	
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	@Column(name="data_atualizacao")
+	private LocalDateTime dataAtualizacao;
+    
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public LocalDateTime getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+    
+    
+	
+}
