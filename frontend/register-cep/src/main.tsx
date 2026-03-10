39cf7f8950b1ae98abe7b6bf7eb8e934dfba8364
andreluislima <andrelimag89@gmail.com>
2026-03-10 20:07:18 -0300

    chore(main): limpa projeto de arquivos desnecessarios e adiciona o strict mode.

--- frontend/register-cep/src/main.tsx
+++ frontend/register-cep/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <StrictMode>
+      <App />
+    </StrictMode>
   </BrowserRouter>,
 );
