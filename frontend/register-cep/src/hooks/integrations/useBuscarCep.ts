7e9ef9c5e09816b0fc52788a18a391d12e88de97
andreluislima <andrelimag89@gmail.com>
2026-03-11 13:49:33 -0300

    feat(integrations): implementa consumo de endpoint buscar cep.

--- frontend/register-cep/src/hooks/integrations/useBuscarCep.ts
+++ frontend/register-cep/src/hooks/integrations/useBuscarCep.ts
@@ -1,8 +1,9 @@
+import { useMutation } from "@tanstack/react-query"
 import { api } from "../../api"
 import type { CepResponse } from "../../types/Usuario.Type"
 
 export const useBuscarCep = () => {
-    return useMutationCep({
+    return useMutation({
         mutationFn: async (cep: string) =>{
             return await api.get<CepResponse>(`/buscarCep/${cep}`)
         }
