2b74fc2a8fa5d2064e7d1c4178dd376263683d6f
andreluislima <andrelimag89@gmail.com>
2026-03-11 02:05:52 -0300

    chore(security): implementa a configuracao do CORS para a liberação de requisições do React.

--- backend/register-cep-api/src/main/java/com/register/cep/api/security/SecurityConfig.java
+++ backend/register-cep-api/src/main/java/com/register/cep/api/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.register.cep.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,15 +15,19 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(
-						authorize -> authorize
-								.requestMatchers(HttpMethod.POST, "/usuario/criarUsuario").permitAll()
-								.requestMatchers(HttpMethod.GET, "/usuario/usuarios").permitAll()
-								.requestMatchers(HttpMethod.PUT, "/usuario/editar/**").permitAll()
-								.requestMatchers(HttpMethod.DELETE, "/usuario/remover/**").permitAll()
-								.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll());
+		http
+			.cors(Customizer.withDefaults())
+			.csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(authorize -> authorize
+					.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+					.requestMatchers(HttpMethod.POST, "/usuario/criarUsuario").permitAll()
+					.requestMatchers(HttpMethod.GET, "/usuario/**").permitAll()
+					.requestMatchers(HttpMethod.PUT, "/usuario/editar/**").permitAll()
+					.requestMatchers(HttpMethod.DELETE, "/usuario/remover/**").permitAll()
+					.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+					.anyRequest().authenticated()
+			);
 
 		return http.build();
 	}
