83ad56043a446a476b0e62105d745844b97ff9df
andreluislima <andrelimag89@gmail.com>
2026-03-13 01:53:50 -0300

    chore(components): limpa componentes sem uso.

--- frontend/register-cep/src/pages/NovoRegistro/NovoRegistro.tsx
+++ frontend/register-cep/src/pages/NovoRegistro/NovoRegistro.tsx
@@ -1,13 +1,11 @@
 
 import { FormShad } from "@/components/Form/FormShad";
-import Form from "../../components/Form/Form";
 import "./NovoRegistro.css";
 
 export default function NovoRegistro() {
   return (
     <>
       <div className="container-novo-registro">
-        {/* <Form/> */}
         <FormShad/>
       </div>
     </>
